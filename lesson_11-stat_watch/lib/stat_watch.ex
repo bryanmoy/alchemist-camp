b60c7f4a6c07559743e2bd04cd002028acf746f7
Bryan Moy <bryanmoy.dev@gmail.com>
2019-06-14 01:14:01 -0700

    Lesson 11

---
+++ lesson_11-stat_watch/lib/stat_watch.ex
@@ -0,0 +1,36 @@
+defmodule StatWatch do
+  def run do
+    fetch_stats()
+    |> save_csv
+  end
+
+  def column_names() do
+    Enum.join(~w(DateTime Subscribers Videos Views), ",")
+  end
+
+  def fetch_stats() do
+    now = DateTime.to_string(%{DateTime.utc_now() | microsecond: {0, 0}})
+
+    %{body: body} = HTTPoison.get!(stats_url())
+    %{items: [%{statistics: stats} | _]} = Poison.decode!(body, keys: :atoms)
+
+    [now, stats.subscriberCount, stats.videoCount, stats.viewCount] |> Enum.join(",e")
+  end
+
+  def save_csv(row_of_stats) do
+    filename = "stats.csv"
+
+    unless File.exists?(filename) do
+      File.write!(filename, column_names() <> "\n")
+    end
+
+    File.write!(filename, row_of_stats <> "\n", [:append])
+  end
+
+  def stats_url() do
+    youtube_api_v3 = "https://www.googleapis.com/youtube/v3/"
+    channel = "id=" <> "UCpG1gFr0tM55OYK3J843Uqw"
+    key = "key=" <> "AIzaSyDTdFqoYfrghTlYswMrRjvT33Rm1E45v5c"
+    "#{youtube_api_v3}channels?#{channel}&#{key}&part=statistics"
+  end
+end
---
+++ lesson_11-stat_watch/mix.exs
@@ -0,0 +1,33 @@
+defmodule StatWatch.MixProject do
+  use Mix.Project
+
+  def column_names() do
+    Enum.join(~w(DateTime Subscribers Videos Views), ",")
+  end
+
+  def project do
+    [
+      app: :stat_watch,
+      version: "0.1.0",
+      elixir: "~> 1.8",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      mod: {StatWatch.Application, []},
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:httpoison, "~> 1.5"},
+      {:poison, "~> 3.1"}
+    ]
+  end
+end
